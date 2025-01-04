5094155ddfed9a5f1730dbf6ab1d4cda9b04013c
Aaron-zon <1640485761@qq.com>
2025-01-04 20:16:09 +0800

    Update blog-page.vue

--- docs/blog/blog-page.vue
+++ docs/blog/blog-page.vue
@@ -12,6 +12,7 @@ console.log('sda', blogRouter)
     <div class="blog-container">
         <ul>
             <li v-for="(blogList, key) in blogRouter" :key="key">
+                <div class="year">{{ key }}</div>
                 <div v-for="blog in blogList" :key="blog.text">
                     {{ blog.text }}
                 </div>
@@ -20,6 +21,31 @@ console.log('sda', blogRouter)
     </div>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
+li {
+    list-style: none;
+}
 
+.year {
+    -webkit-text-stroke-width: 2px;
+    -webkit-text-stroke-color: rgb(170, 170, 170);
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    font-feature-settings: normal;
+    font-size: 128px;
+    font-variation-settings: normal;
+    font-weight: 700;
+    line-height: 224px;
+    list-style-image: none;
+    list-style-position: outside;
+    list-style-type: none;
+    scrollbar-color: rgb(238, 238, 238) rgb(255, 255, 255);
+    tab-size: 4;
+    text-size-adjust: 100%;
+    position: absolute;
+    top: -32px;
+    left: -48px;
+    opacity: .1;
+    color: transparent;
+
+}
 </style>
