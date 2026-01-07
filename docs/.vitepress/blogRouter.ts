5cd9109f4a438cd11035dec8d229519fef8d3d8a
dl_zhoujunyang <1640485761@qq.com>
2026-01-07 17:50:59 +0900

    add

--- docs/.vitepress/blogRouter.ts
+++ docs/.vitepress/blogRouter.ts
@@ -10,5 +10,6 @@ export const blogRouter = {
     { text: 'SpringBoot FormData 参数个数限制', link: 'springboot-formdata-limit'},
     { text: 'SqlServer 导出表结构和数据', link: 'sqlserver-export-table'},
     { text: 'IDEA 启动一个 Tomcat 项目', link: 'idea-start-tomcat'},
+    { text: '常见的企业信息化系统（OA/ERP/MES/WMS）', link: 'enterprise-system'}
   ],
 }
