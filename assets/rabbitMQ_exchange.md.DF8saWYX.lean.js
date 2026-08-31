7eec67e875d85297040ebf40491722a87754bf8f
Aaron-zon <Aaron-zon@users.noreply.github.com>
2026-08-31 02:50:21 +0000

    deploy: 2d56d28abe828c553fe8aadee56a8ed9c0f0a8e6

---
+++ assets/rabbitMQ_exchange.md.DF8saWYX.lean.js
@@ -0,0 +1,87 @@
+import{ax as n,A as s,aQ as p,z as e}from"./chunks/framework.BWRXlwpK.js";const u=JSON.parse('{"title":"Exchange","description":"","frontmatter":{},"headers":[],"relativePath":"rabbitMQ/exchange.md","filePath":"rabbitMQ/exchange.md","lastUpdated":1788144574000}'),i={name:"rabbitMQ/exchange.md"};function l(c,a,t,o,d,r){return e(),s("div",null,a[0]||(a[0]=[p(`<h1 id="exchange" tabindex="-1">Exchange <a class="header-anchor" href="#exchange" aria-label="Permalink to &quot;Exchange&quot;">​</a></h1><p>先回顾一下架构</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Producer</span></span>
+<span class="line"><span>    │</span></span>
+<span class="line"><span>    ▼</span></span>
+<span class="line"><span>Exchange</span></span>
+<span class="line"><span>    │</span></span>
+<span class="line"><span>    ▼</span></span>
+<span class="line"><span>Queue</span></span>
+<span class="line"><span>    │</span></span>
+<span class="line"><span>    ▼</span></span>
+<span class="line"><span>Consumer</span></span></code></pre></div><p>之前的章节我们知道</p><ul><li>Producer 不能直接发送 Queue</li><li>Consumer 不监听 Exchange</li><li>Exchange 不保存消息，只负责分配</li></ul><p>Producer 把消息交给 Exchange后</p><blockquote><p>发给谁，发几个，发那里 全部交给了 Exchange</p></blockquote><p>所以</p><blockquote><p>Exchange 就是消息路由器（Router）</p></blockquote><h2 id="exchange-的组成" tabindex="-1">Exchange 的组成 <a class="header-anchor" href="#exchange-的组成" aria-label="Permalink to &quot;Exchange 的组成&quot;">​</a></h2><p>Exchange 主要有三个东西：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Exchange</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>名字(Name)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>类型(Type)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>绑定(Binding)</span></span></code></pre></div><p>例如：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Exchange</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>name = order.exchange</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>type = direct</span></span></code></pre></div><p>然后：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>绑定：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Queue_A</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>RoutingKey=order.create</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Queue_B</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>RoutingKey=order.pay</span></span></code></pre></div><h2 id="binding-绑定" tabindex="-1">Binding（绑定） <a class="header-anchor" href="#binding-绑定" aria-label="Permalink to &quot;Binding（绑定）&quot;">​</a></h2><p>Binding 就是把 Exchange 和 Queue 关联起来。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Exchange ———— Binding ———— Queue</span></span></code></pre></div><p>绑定时需要指定 <code>Binding Key</code></p><p>例如：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Queue_Order</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>BindingKey</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>order.*</span></span></code></pre></div><p>以后 Exchange 收到消息时，会判断消息的 <code>RoutingKey</code> 是否匹配 <code>BindingKey</code>。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>RoutingKey</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>order.create</span></span></code></pre></div><p>如上这个Routing Key</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>order.*</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>匹配</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Queue_Order</span></span></code></pre></div><p>发现可以匹配，那么就将这个消息发送给 Queue_Order</p><p>Binding就是 <strong>路由规则</strong></p><h2 id="routing-key" tabindex="-1">Routing Key <a class="header-anchor" href="#routing-key" aria-label="Permalink to &quot;Routing Key&quot;">​</a></h2><p>Producer 发消息的时候会带一个 <code>Routing Key</code></p><p>例如：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>channel.basicPublish(</span></span>
+<span class="line"><span>    &quot;order.exchange&quot;,</span></span>
+<span class="line"><span>    &quot;order.create&quot;,</span></span>
+<span class="line"><span>    null,</span></span>
+<span class="line"><span>    body</span></span>
+<span class="line"><span>);</span></span></code></pre></div><p>这里 <code>order.create</code> 就是 \`Routing Key</p><p>Exchange根据 Routing Key 和 Binding Key 决定这条消息究竟去哪个 Queue。</p><h2 id="四种-exchange" tabindex="-1">四种 Exchange <a class="header-anchor" href="#四种-exchange" aria-label="Permalink to &quot;四种 Exchange&quot;">​</a></h2><p>RabbitMQ 内置四种匹配机制。</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Direct</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Fanout</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Topic</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Headers</span></span></code></pre></div><p>我们主要使用前三种，Headers 基本不用。</p><h3 id="direct-exchange-精确匹配" tabindex="-1">Direct Exchange（精确匹配） <a class="header-anchor" href="#direct-exchange-精确匹配" aria-label="Permalink to &quot;Direct Exchange（精确匹配）&quot;">​</a></h3><p>要求 <strong>Routing Key === Binding Key</strong></p><p>如果 Queue_A 的 <code>Binding = order.create</code></p><p>Producer 发送消息时 <code>RoutingKey = order.create</code>，那么它就可以进入 Queue_A。</p><p>如果 <code>RoutingKey = order.pay</code> 由于不完全匹配，所以不会进入 Queue_A。</p><h3 id="fanout-exchange-广播" tabindex="-1">Fanout Exchange（广播） <a class="header-anchor" href="#fanout-exchange-广播" aria-label="Permalink to &quot;Fanout Exchange（广播）&quot;">​</a></h3><p>不看 Routing Key，受到消息，全部发送。</p><p>受到消息后将消息发送到匹配规则为Fanout Exchange 的所有 Queue。</p><p>它适合：</p><ul><li>系统通知</li><li>配置刷新</li><li>广播消息</li></ul><h3 id="topic-exchange-通配符" tabindex="-1">Topic Exchange（通配符） <a class="header-anchor" href="#topic-exchange-通配符" aria-label="Permalink to &quot;Topic Exchange（通配符）&quot;">​</a></h3><p>这是功能性最强的匹配方式</p><p>它支持 <code>*</code> 和 <code>#</code></p><p>其中 <code>*</code> 标识匹配一个单词。</p><p>例如：</p><p><code>order.*</code> 可以匹配</p><ul><li>order.create</li><li>order.pay</li></ul><p>但是不可以匹配</p><ul><li>prder.pay.success</li></ul><p>因为多了一级。</p><p><code>#</code> 表示匹配多个单词。</p><p>例如：</p><p><code>order.#</code> 可以匹配</p><ul><li>order.create</li><li>order.pay</li><li>order.pay.success</li><li>order.xxx.yyy.zzz</li></ul><h3 id="headers-exchange" tabindex="-1">Headers Exchange <a class="header-anchor" href="#headers-exchange" aria-label="Permalink to &quot;Headers Exchange&quot;">​</a></h3><p>不用 Routing Key，而是根据</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Header</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>color=red</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>type=pdf</span></span></code></pre></div><p>匹配，性能较差，一般不用。</p><h2 id="完整流程" tabindex="-1">完整流程 <a class="header-anchor" href="#完整流程" aria-label="Permalink to &quot;完整流程&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Producer</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      │</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Message</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>RoutingKey</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      │</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      ▼</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Exchange</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      │</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>查 Binding</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      │</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>找到 Queue</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      │</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>投递</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      ▼</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Queue</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>      │</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Consumer</span></span></code></pre></div><blockquote><p>Exchange 是 RabbitMQ 的消息路由中心。Producer 将消息发送给 Exchange，Exchange 根据自身类型（Direct、Fanout、Topic、Headers）以及 Binding Key 和 Routing Key 的匹配关系，将消息路由到一个或多个 Queue。Exchange 自身不存储消息，只负责路由，真正存储消息的是 Queue。</p></blockquote>`,69)]))}const g=n(i,[["render",l]]);export{u as __pageData,g as default};
---
+++ assets/rabbitMQ_rabbitmq-architecture.md.D18-EfdZ.js
@@ -0,0 +1,107 @@
+import{ax as a,A as n,aQ as p,z as e}from"./chunks/framework.BWRXlwpK.js";const h=JSON.parse('{"title":"整体架构","description":"","frontmatter":{},"headers":[],"relativePath":"rabbitMQ/rabbitmq-architecture.md","filePath":"rabbitMQ/rabbitmq-architecture.md","lastUpdated":1788144574000}'),l={name:"rabbitMQ/rabbitmq-architecture.md"};function i(c,s,t,o,r,u){return e(),n("div",null,s[0]||(s[0]=[p(`<h1 id="整体架构" tabindex="-1">整体架构 <a class="header-anchor" href="#整体架构" aria-label="Permalink to &quot;整体架构&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>                RabbitMQ Broker</span></span>
+<span class="line"><span>┌──────────────────────────────────────────────┐</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│ Producer（生产者）                            │</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│    ↓                                         │</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│ Exchange（交换机）                            │</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│    ↓                                         │</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│ Queue（队列）                                 │</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│    ↓                                         │</span></span>
+<span class="line"><span>│                                              │</span></span>
+<span class="line"><span>│ Consumer（消费者）                            │</span></span>
+<span class="line"><span>└──────────────────────────────────────────────┘</span></span></code></pre></div><blockquote><p>RabbitMQ 的所有内容都是围绕这五个角色。</p></blockquote><ul><li>Producer</li><li>Exchange</li><li>Queue</li><li>Consumer</li><li>Broker</li></ul><h2 id="producer-生产者" tabindex="-1">Producer（生产者） <a class="header-anchor" href="#producer-生产者" aria-label="Permalink to &quot;Producer（生产者）&quot;">​</a></h2><blockquote><p>发消息的人（程序）</p></blockquote><p>例如：</p><p>一个电商系统，用户点击下单。</p><div class="language-java vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">java</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">rabbitTemplate.</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">convertAndSend</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(</span></span>
+<span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">    &quot;order.exchange&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">    &quot;order.create&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    order</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">);</span></span></code></pre></div><p>Producer做了两件事情</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>发送消息</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>告诉 RabbitMQ：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Exchange 是谁？</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>RoutingKey 是什么？</span></span></code></pre></div><h2 id="exchange-交换机" tabindex="-1">Exchange（交换机） <a class="header-anchor" href="#exchange-交换机" aria-label="Permalink to &quot;Exchange（交换机）&quot;">​</a></h2><blockquote><p>决定消息应该进入哪个 Queue。</p></blockquote><p>它是 RabbitMQ的大脑，它负责：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>收到消息</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>根据规则判断</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>转发给 Queue</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>自己结束工作</span></span></code></pre></div><p>例如：</p><p>当他收到</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>routingKey = order.create</span></span></code></pre></div><p>Exchange:</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>查看绑定规则</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>应该去</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Queue A</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>转发过去</span></span></code></pre></div><p>他像一个领导者，决定消息应该去哪。</p><h2 id="queue-队列" tabindex="-1">Queue（队列） <a class="header-anchor" href="#queue-队列" aria-label="Permalink to &quot;Queue（队列）&quot;">​</a></h2><blockquote><p>消息仓库，存储消息的地方。</p></blockquote><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Queue</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>-------------------</span></span>
+<span class="line"><span>订单1</span></span>
+<span class="line"><span>订单2</span></span>
+<span class="line"><span>订单3</span></span>
+<span class="line"><span>订单4</span></span>
+<span class="line"><span>订单5</span></span>
+<span class="line"><span>-------------------</span></span></code></pre></div><p>进入 Queue 后，即使 Consumer 不在线，消息也可以继续待在 Queue 中。</p><p>例如：</p><p>Producer发来了10000条消息，但是 Consumer 还在忙或没启动，这时这10000条数据就会先保存在 Queue。</p><p>等到 Consumer 可以正常工作了，再开始将 Queue 中的消息取出，一个一个的交给 Consumer 处理。</p><p>Queue的主要作用就是存储消息。</p><h2 id="consumer-消费者" tabindex="-1">Consumer（消费者） <a class="header-anchor" href="#consumer-消费者" aria-label="Permalink to &quot;Consumer（消费者）&quot;">​</a></h2><blockquote><p>处理消息的程序</p></blockquote><p>例如：</p><p>订单服务收到了创建订单的请求，接下来的工作是</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>写数据库</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>扣库存</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>发短信</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>结束</span></span></code></pre></div><p>Consumer 会一直监听 Queue，只要 Queue 有消息就会：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Queue</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Consumer</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>处理</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>ACK（消费者告诉 RabbitMQ：&quot;这条消息我已经成功处理了，你可以把它删掉了。&quot;）</span></span></code></pre></div><h2 id="为什么一定要-exchange" tabindex="-1">为什么一定要 Exchange？ <a class="header-anchor" href="#为什么一定要-exchange" aria-label="Permalink to &quot;为什么一定要 Exchange？&quot;">​</a></h2><p>很多人第一次都会想：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Producer</span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span>Queue</span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span>Consumer</span></span></code></pre></div><p>不是更简单吗？</p><p>RabbitMQ 之所以设计 Exchange，是为了 <strong>解耦</strong>。</p><p>假设有三个系统：订单、库存、短信</p><p>用户下单需要：创建订单、口库存、发短信</p><p>如果没有Exchange：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Producer</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>├── Queue1</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>├── Queue2</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>└── Queue3</span></span></code></pre></div><p>Producer 必须知道：</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>三个 Queue</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>三个名字</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>三个地址</span></span></code></pre></div><p>以后如果增加了 <code>积分系统</code>，Producer 又要修改代码。增加 <code>物流系统</code> 也要修改 Producer 的代码。</p><p>这样就形成了 <strong>强耦合</strong>。</p><p>有了 Exchange 后</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>Producer</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>↓</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Exchange</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>├── Queue1</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>├── Queue2</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>├── Queue3</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>└── Queue4</span></span></code></pre></div><p>Producer 永远只需要发送给 Exchange。</p><p>以后新增 Queue 不需要修改 Producer，Exchange 增加绑定即可。</p><p>这就是 Exchange 最大的价值：</p><blockquote><p>发送方和接收方完全解耦，系统扩展性大大增强。</p></blockquote><h2 id="broker" tabindex="-1">Broker <a class="header-anchor" href="#broker" aria-label="Permalink to &quot;Broker&quot;">​</a></h2><p>Broker 就是整个 RabbitMQ 服务（RabbitMQ Server）</p>`,57)]))}const b=a(l,[["render",i]]);export{h as __pageData,b as default};
