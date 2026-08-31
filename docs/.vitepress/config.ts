9ac64899855d600a6ada765095bc56a44652beca
dl_zhoujunyang <1640485761@qq.com>
2026-08-31 10:46:13 +0800

    qa

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -606,17 +606,17 @@ export const sidebar: ThemeConfig['sidebar'] = {
       ]
     }
   ],
-  '/qa': [
-    {
-      text: 'qa',
-      items: [
-        { text: 'vue', link: '/qa/vue' },
-        { text: 'react', link: '/qa/react' },
-        { text: 'java', link: '/qa/java' },
-        { text: 'java', link: '/qa/project' },
-      ]
-    }
-  ]
+  // '/qa': [
+  //   {
+  //     text: 'qa',
+  //     items: [
+  //       { text: 'vue', link: '/qa/vue' },
+  //       { text: 'react', link: '/qa/react' },
+  //       { text: 'java', link: '/qa/java' },
+  //       { text: 'java', link: '/qa/project' },
+  //     ]
+  //   }
+  // ]
 }
 
 // https://vitepress.dev/reference/site-config
